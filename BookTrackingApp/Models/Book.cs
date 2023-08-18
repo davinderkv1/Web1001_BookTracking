d77500663f673a064818c6b14b407dafe7b44d3e
davinderkv1 <davinderkv1@rediffmail.com>
2023-08-18 13:57:48 -0700

    Create the data entry pages by scaffolding

--- BookTrackingApp/Models/Book.cs
+++ BookTrackingApp/Models/Book.cs
@@ -1,5 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Book
 {
+    [Key] // This attribute specifies that 'Id' is the primary key
+    public int Id { get; set; } // Primary key
+
     public string ISBN { get; set; }
     public string Title { get; set; }
     public int CategoryId { get; set; }
